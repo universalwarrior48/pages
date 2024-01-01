6abcee95f6c0a1b7b74839371ca9cf1d95f8248b
Sagar Pohekar <sagarpohekar@gmail.com>
2024-01-01 12:17:43 +0530

    Vercerl Deployment SQLite

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,19 +76,19 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "URL": config("POSTGRES_URL"),
-        "NAME": config("PGNAME"),
-        "USER": config("PGUSER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("PGHOST"),
-        "PORT": config("PGPORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "URL": config("POSTGRES_URL"),
+    #     "NAME": config("PGNAME"),
+    #     "USER": config("PGUSER"),
+    #     "PASSWORD": config("POSTGRES_PASSWORD"),
+    #     "HOST": config("PGHOST"),
+    #     "PORT": config("PGPORT"),
+    # }
 }
 
 
